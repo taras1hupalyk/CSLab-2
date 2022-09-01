12779d9eb43fe23152816357fc262baa6cc77cc5
taras1hupalyk <t.hupalyk@gmail.com>
2022-09-01 13:01:16 +0300

    Add project files.

---
+++ Lab-2.Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Lab_2.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, input the first number: ");
+            var firstNumber = Console.ReadLine();
+            Console.WriteLine("And now, enter the second: ");
+            var secondNumber = Console.ReadLine();
+            Multiplication(firstNumber, secondNumber);
+            Console.ReadKey();
+        }
+
+        public static void Multiplication(string firstNumber, string secondNumber)
+        {
+            Int64 multiplicand, multiplier, product = 0, startMultiplicand, startMultiplier;
+            startMultiplicand = multiplicand = Int32.Parse(firstNumber);
+            startMultiplier = multiplier = Int32.Parse(secondNumber);
+            for (int i = 0; i < 32; ++i)
+            {
+                Console.WriteLine("The step № " + (i + 1) + ":\n");
+
+                Console.WriteLine("Multiplicand:\n" + FinishStringWithZeros(Convert.ToString(multiplicand, 2)) +
+                    "\nMultiplier:\n" + FinishStringWithZeros(Convert.ToString(multiplier, 2)) + "\n");
+
+                short lsb = (short)(multiplier & 1);
+
+                if (lsb == 1)
+                {
+                    Console.WriteLine("Add multiplicand and product.\n");
+                    product += multiplicand;
+                }
+                Console.WriteLine("Product:\n" + FinishStringWithZeros(Convert.ToString(product, 2)) + "\n");
+                Console.WriteLine("Shift multiplicand left");
+                Console.WriteLine("Shift multiplier right");
+
+                multiplicand <<= 1;
+                multiplier >>= 1;
+            }
+
+            Console.WriteLine("\n" + FinishStringWithZeros(Convert.ToString(startMultiplicand, 2)) +
+                "\nx\n" +
+                FinishStringWithZeros(Convert.ToString(startMultiplier, 2)) +
+                "\n=\n" +
+                FinishStringWithZeros(Convert.ToString(product, 2)) + "\n");
+
+            Console.WriteLine(startMultiplicand + " x " + startMultiplier + " = " + product);
+        }
+
+        static string FinishStringWithZeros(string val)
+        {
+            var count = 64 - val.Length;
+            var head = "";
+            for (int i = 0; i < count; ++i)
+                head += "0";
+            return head + val;
+        }
+    }
+}
---
+++ Lab-2.Task3/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Lab_2.Task3
+{
+    internal class Program
+    {
+        public static uint a_main = 0b011110100100000000000000;
+        public static uint b_main = 0b010000010000000000000000;
+        public static uint a_exponenta = 0b10000101;
+        public static uint b_exponenta = 0b10000010;
+        public static uint a_znack = 0;
+        public static uint b_znack = 0;
+        public static uint fin_znack = 0;
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("a = ");
+            float a = float.Parse(Console.ReadLine());
+            uint A = (uint)BitConverter.SingleToInt32Bits(a);
+
+            Console.Write("b = ");
+            float b = float.Parse(Console.ReadLine());
+            uint B = (uint)BitConverter.SingleToInt32Bits(b);
+            Console.WriteLine();
+            a_znack = A >> 31;
+            b_znack = B >> 31;
+
+            a_exponenta = (A >> 23) & 0xFF;
+            b_exponenta = (B >> 23) & 0xFF;
+            a_main = A & 0x007FFFFF;
+            b_main = B & 0x007FFFFF;
+
+
+            fin_znack = a_znack ^ b_znack;
+            Console.WriteLine("Результуючий біт знаку= " + Convert.ToString(fin_znack, 2)); // 1 - мінус; 0 - плюс
+            uint exp = a_exponenta + b_exponenta - 0x7F;
+            Console.WriteLine("Основна експонента результату: (" + Convert.ToString(a_exponenta, 2) + " ) + ( " + Convert.ToString(b_exponenta, 2) + " ) = ( " + Convert.ToString(exp, 2) + " )");
+            a_main |= 0x00800000; // побітове або
+            b_main |= 0x00800000;
+            ulong zFrac = (ulong)a_main * (ulong)b_main;
+
+            uint zFrac0 = (uint)(zFrac >> 24);
+
+
+            Console.WriteLine("Основна мантиса: (" + Convert.ToString(a_main, 2) + " ) * ( " + Convert.ToString(b_main, 2) + " )  = ( " + Convert.ToString((long)zFrac, 2) + " )");
+            Console.WriteLine("Нормалізований результат");
+            if (zFrac0 >> 22 == 0b1)
+            {
+                Console.WriteLine("Результуюча мантиса 1, тому Мантиса зсувається вліво на одиницю");
+                zFrac0 &= 0x3FFFFF;
+
+                zFrac0 <<= 1;
+            }
+            else
+            {
+                Console.WriteLine("Результуюча мантиса  0, тому додаємо до експоненти 1");
+                zFrac0 &= 0x3FFFFF;
+                exp++;
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Результат");
+            Console.WriteLine("Знак: " + Convert.ToString(fin_znack, 2));
+            Console.WriteLine("Експонента: " + Convert.ToString(exp, 2));
+            Console.WriteLine("Мантиса: " + Convert.ToString(zFrac0, 2));
+            uint final = zFrac0 + (exp << 23) + (fin_znack << 31);
+            float c = BitConverter.Int32BitsToSingle((int)final);
+            Console.WriteLine("Кінцевий десятковий результат " + c);
+
+        }
+
+    }
+}
